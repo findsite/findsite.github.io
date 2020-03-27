cfcf962b002ab43cbb63d31b90a3c5ec694c217d
findsite <60389517+findsite@users.noreply.github.com>
2020-03-27 03:17:21 +0200

    Update findsite.js

--- findsite.js
+++ findsite.js
@@ -1,10 +1,25 @@
-function error() {
+window.onload = function(){
+	let hhr = document.getElementById("hhr");
+	hhr.style = "animation: animhhr 1s forwards .15s";
+	let htheader = document.getElementById("htheader");
+	htheader.style = "animation: animhtheader .8s forwards 1115ms";
+}
+
+function setStandartProperty(){
+	let hhr = document.getElementById("hhr");
+	hhr.style.width = "85%";
+	let htheader = document.getElementById("htheader");
+	htheader.style.opacity = "1";
+}
+
+function error(){
       alert("Данный сайт находится в разработке");
 }
-function errorr() {
+function errorr(){
 	  alert("Данный раздел находится в разработке");
 }
-check = 0;
+
+let check = 0;
 function darkover(){
 let dq = document.getElementById("html" );
 let dw = document.getElementById("header" );
@@ -15,8 +30,8 @@ let dy = document.getElementById("rhr" );
 let dz = document.getElementById("horazdelu");
 let dao = document.getElementById("staso");
 let dat = document.getElementById("stast");
-    dat.href = "dhimia.html";
-    dao.href = "dguitar.html";
+    dat.href = "dguitar.html";
+    dao.href = "dhimia.html";
 	dq.style = "animation: adhtml .3s forwards";
 	dw.style = "animation: adheader .3s forwards";
 	de.style = "animation: adhhr .3s forwards";
@@ -24,8 +39,10 @@ let dat = document.getElementById("stast");
     dt.style = "animation: adpheader .3s forwards";
 	dy.style = "animation: adrhr .3s forwards";	
 	dz.style = "animation: adhorazdel .3s forwards";
+	setStandartProperty();
 }
-function darkout() {
+
+function darkout(){
 if(check == 0){
 let dq = document.getElementById("html" );
 let dw = document.getElementById("header" );
@@ -34,10 +51,6 @@ let dr = document.getElementById("htheader" );
 let dt = document.getElementById("pheader" );
 let dy = document.getElementById("rhr" );
 let dz = document.getElementById("horazdelu");
-let dao = document.getElementById("staso");
-let dat = document.getElementById("stast");
-    dat.href = "himia.html";
-    dao.href = "guitara.html";
 	dq.style = "animation: adhtmlrev .3s forwards";
 	dw.style = "animation: adheaderrev .3s forwards";
 	de.style = "animation: adhhrrev .3s forwards";
@@ -45,9 +58,11 @@ let dat = document.getElementById("stast");
     dt.style = "animation: adpheaderrev .3s forwards";
 	dy.style = "animation: adrhrrev .3s forwards";	
 	dz.style = "animation: adhorazdelrev .3s forwards";
+	setStandartProperty();
 }
 }
-function lightover() {
+
+function lightover(){
 let dq = document.getElementById("html" );
 let dw = document.getElementById("header" );
 let de = document.getElementById("hhr" );
@@ -55,10 +70,6 @@ let dr = document.getElementById("htheader" );
 let dt = document.getElementById("pheader" );
 let dy = document.getElementById("rhr" );
 let dz = document.getElementById("horazdelu");
-let dao = document.getElementById("staso");
-let dat = document.getElementById("stast");
-    dat.href = "himia.html";
-    dao.href = "guitara.html";
 	dq.style = "animation: adhtmlrev .3s forwards";
 	dw.style = "animation: adheaderrev .3s forwards";
 	de.style = "animation: adhhrrev .3s forwards";
@@ -66,8 +77,10 @@ let dat = document.getElementById("stast");
     dt.style = "animation: adpheaderrev .3s forwards";
 	dy.style = "animation: adrhrrev .3s forwards";	
 	dz.style = "animation: adhorazdelrev .3s forwards";	
+	setStandartProperty();
 }
-function lightout() {
+
+function lightout(){
 if(check == 1) {
 let dq = document.getElementById("html" );
 let dw = document.getElementById("header" );
@@ -76,10 +89,6 @@ let dr = document.getElementById("htheader" );
 let dt = document.getElementById("pheader" );
 let dy = document.getElementById("rhr" );
 let dz = document.getElementById("horazdelu");
-let dao = document.getElementById("staso");
-let dat = document.getElementById("stast");
-    dat.href = "dhimia.html";
-    dao.href = "dguitar.html";
 	dq.style = "animation: adhtml .3s forwards";
 	dw.style = "animation: adheader .3s forwards";
 	de.style = "animation: adhhr .3s forwards";
@@ -87,5 +96,34 @@ let dat = document.getElementById("stast");
     dt.style = "animation: adpheader .3s forwards";
 	dy.style = "animation: adrhr .3s forwards";	
 	dz.style = "animation: adhorazdel .3s forwards";	
+	setStandartProperty();
 }
 }
+function descrone(x){
+	if(x == 1){
+		let descro = document.getElementById("descrone");
+		descro.style = "animation: descr .5s forwards";
+	}
+	if (x == 2){
+		let descrt = document.getElementById("descrtwo");
+		descrt.style = "animation: descr .5s forwards";
+	}
+	if (x == 3) {
+		let descrth = document.getElementById("descrthree");
+		descrth.style = "animation: descr .5s forwards";
+	}
+}
+function descrzero(x){
+	if(x == 1){
+		let descro = document.getElementById("descrone");
+		descro.style = "animation: descrrevers .5s forwards";
+	}
+	if (x == 2){
+		let descrt = document.getElementById("descrtwo");
+		descrt.style = "animation: descrrevers .5s forwards";
+	}
+	if (x == 3) {
+		let descrth = document.getElementById("descrthree");
+		descrth.style = "animation: descrrevers .5s forwards";
+	}
+}	
