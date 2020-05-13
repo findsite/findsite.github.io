23398c8c363539b43e2837ccacc8e287ed6656cf
findsite <60389517+findsite@users.noreply.github.com>
2020-05-13 13:40:23 +0300

    Update discriminant.js

--- discriminant.js
+++ discriminant.js
@@ -199,6 +199,9 @@ function setCoef() {
                         document.getElementById(lines[15]).innerHTML = "<br/>X1 * X2 = "+c;
                         document.getElementById(lines[16]).innerHTML = "X1 = "+x_one+"<br/>X2 = "+x_two;
                     }
+		    else {
+			document.getElementById(lines[14]).innerHTML = "<br/><br/>Невозможно решить через теорему Виета, так-как коэффициент 'a' не равен 1.    
+		    }
             }
             else {
                     document.getElementById(lines[3]).innerHTML = "Корней нет";
