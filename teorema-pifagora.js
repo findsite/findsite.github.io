22733ce5b2d091f182d0862c903b7807d75a8925
findsite <60389517+findsite@users.noreply.github.com>
2021-03-14 15:41:30 +0200

    Update teorema-pifagora.js

--- teorema-pifagora.js
+++ teorema-pifagora.js
@@ -1,81 +1,110 @@
-let lines = new Array("fr-line", "sc-line", "tr-line", "fu-line", "fv-line", "sx-line", "sv-line", "eg-line", "nn-line", "tn-line", "el-line", "tw-line", "tn-line", "fn-line", "sn-line", "en-line");   
-function setCoef() {
-		let a = prompt("Введите длину гипотенузы (неизвестную сторону обозначте за 0):");
-		let b = prompt("Введите длину одного катета (неизвестную сторону обозначте за 0):");
-		let c = prompt("Введите длину третьего катета (неизвестную сторону обозначте за 0):");
-    let sqr = "²";
-	let no_radical = "Корня нет. Число не подходит по смыслу задачи (длина стороны не может быть отрицательной либо равной 0)";
-    let sign = new Array(" = ", " + ", " - ");
-        document.getElementById(lines[0]).innerHTML = "<br/>AB"+sqr+sign[0]+"BC"+sqr+sign[1]+"AC"+sqr+"<br/><br/>";
-    a *= a;
-    b *= b;
-    c *= c;
-    let i, radical = 0;
-	let bpc = b + c;
-			document.getElementById(lines[1]).innerHTML = "AB"+sqr+sign[0]+b+sign[1]+c+sign[0]+bpc+"<br/>"+"AB"+sign[0];
-		for(i = 0; i < 15000; i++) {
-			if(i*i == b+c) {
-				radical = i;
-				break;
+$(function() {
+	$('#start').click(function() { ($('#katet1').val() != '' && $('#katet2').val() != '' && $('#hypoten').val() != '') ? start() : alert('Заполните необходимые поля') });
+
+
+	function findUnknownSide(returnType) {
+		if($('#katet1').val() % 1 != 0) {
+			return (returnType == 'value' ? [0, 2] : 'Katet1');
+		}
+		if($('#katet2').val() % 1 != 0) {
+			return (returnType == 'value' ? [2, 1] : 'Katet2');
+		}
+		if($('hypoten').val() % 1 != 0) {
+			return (returnType == 'value' ? [0, 1] : 'Hypoten'); 
+		}
+	}
+
+	function radical(num, t = true) {
+		for(let i = num; i > 1; i--) {
+			if(i * i == num) {
+				return i;
 			}
-		}  
-    if(b+c > 0) {		
-		if(radical > 0) {
-				document.getElementById(lines[2]).innerHTML = radical;
 		}
-		else {
-				document.getElementById(lines[2]).innerHTML = "√"+bpc;
+		return t ? radicalDecay(num) : -1;
+	}
+	function radicalDecay(num) {
+		for(let i = num - 1; i > 1; i--) {
+			if(Math.pow(radical(num / i, false), 2) * i == num) {
+				return radical(num / i) + '√' + i;
+			}
 		}
+		return '√' + num;
 	}
-	else {
-		document.getElementById(lines[1]).innerHTML = no_radical;
+
+	function start() {
+		
+		let x = {
+			value: findUnknownSide('value'),
+			type: findUnknownSide('type')
+		}
+		//console.log(x, xType);
+
+		let solution = $('#solution');
+		let text = '';
+
+		solution.css('opacity', '1');
+
+		let sides = [
+			$('#triangleName').val()[0] != undefined ? $('#triangleName').val()[0].toString() : 'A', // Первый катет
+			$('#triangleName').val()[1] != undefined ? $('#triangleName').val()[1].toString() : 'B', // Второй катет
+			$('#triangleName').val()[2] != undefined ? $('#triangleName').val()[2].toString() : 'C', // Гипотенуза
+		];
+		//console.log(sides);
+
+		let s1 = document.getElementById('a') ? document.getElementById('a') : document.createElement('p');
+		s1.innerHTML = sides[0];
+		s1.className = 'letter';
+		s1.id = 'a';
+
+		let s2 = document.getElementById('b') ? document.getElementById('b') : document.createElement('p');
+		s2.innerHTML = sides[1];
+		s2.className = 'letter';
+		s2.id = 'b';
+		
+		let s3 = document.getElementById('c') ? document.getElementById('c') : document.createElement('p');
+		s3.innerHTML = sides[2];
+		s3.className = 'letter';
+		s3.id = 'c';
+
+		$('#triangle').append(s1, s2, s3);
+
+		let k = '<sup>2</sup>';
+
+		text += sides[0] + sides[1] + k + ' = ' + sides[0] + sides[2] + k + ' + ' + sides[1] + sides[2] + k;
+
+		if(x.type == 'Katet1') {
+			text += '<br/>' + sides[x.value[0]] + sides[x.value[1]] + k + ' = ';
+			text += sides[0] + sides[1] + k + ' - ' + sides[1] + sides[2] + k + ' = ' + Math.pow($('#hypoten').val(), 2) + ' - ' + Math.pow($('#katet2').val(), 2) + ' = ' + (Math.pow($('#hypoten').val(), 2) - Math.pow($('#katet2').val(), 2)) + '<br/>';
+			if(Math.pow($('#hypoten').val(), 2) - Math.pow($('#katet2').val(), 2) <= 0) {
+				text += '<br/>Длина не может быть меньше либо равной нулю, соотвественно треугольник с введёнными сторонами не может существовать.';
+			}
+			else {
+				text += sides[x.value[0]] + sides[x.value[1]] + ' = ' + radical(Math.pow($('#hypoten').val(), 2) - Math.pow($('#katet2').val(), 2)) + ' (см.)';
+			}
+
+		}
+		if(x.type == 'Katet2') {
+			text += '<br/>' + sides[x.value[0]] + sides[x.value[1]] + k + ' = ';
+			text += sides[0] + sides[1] + k + ' - ' + sides[0] + sides[2] + k + ' = ' + Math.pow($('#hypoten').val(), 2) + ' - ' + Math.pow($('#katet1').val(), 2) + ' = ' + (Math.pow($('#hypoten').val(), 2) - Math.pow($('#katet1').val(), 2)) + '<br/>';
+			if(Math.pow($('#hypoten').val(), 2) - Math.pow($('#katet1').val(), 2) <= 0) {
+				text += '<br/>Длина не может быть меньше либо равной нулю, соотвественно треугольник с введёнными сторонами не может существовать.';
+			}
+			else {
+				text += sides[x.value[0]] + sides[x.value[1]] + ' = ' + radical(Math.pow($('#hypoten').val(), 2) - Math.pow($('#katet1').val(), 2)) + ' (см.)';
+			}
+
+		}
+		if(x.type == 'Hypoten') {
+			text += ' = ' + Math.pow($('#katet1').val(), 2) + ' + ' + Math.pow($('#katet2').val(), 2) + ' = ' + (Math.pow($('#katet1').val(), 2) + Math.pow($('#katet2').val(), 2)) + '<br/>';
+			if(Math.pow($('#katet1').val(), 2) + Math.pow($('#katet2').val(), 2) <= 0) {
+				text += '<br/>Длина не может быть меньше либо равной нулю, соотвественно треугольник с введёнными сторонами не может существовать.';
+			}
+			else {
+				text += sides[x.value[0]] + sides[x.value[1]] + ' = ' + radical(Math.pow($('#katet1').val(), 2) + Math.pow($('#katet2').val(), 2)) + ' (см.)';
+			}
+
+		}
+
+		solution.html(text);
 	}
-	let amc = a - c;
-        document.getElementById(lines[4]).innerHTML = "<br/><br/>BC"+sqr+sign[0]+a+sign[2]+c+sign[0]+amc+"<br/>BC"+sign[0];
-    if(a-c > 0) {
-        radical = 0;
-        for(i = 0; i < 1000; i++) {
-            if(i*i == a-c) {
-               radical = i;
-               break; 
-            }
-        }
-        if(radical > 0) {
-                document.getElementById(lines[5]).innerHTML = radical;
-        }
-        else {
-                document.getElementById(lines[5]).innerHTML = "√"+amc;
-        }
-    }
-    else {
-            document.getElementById(lines[5]).innerHTML = no_radical;
-    }
-        document.getElementById(lines[6]).innerHTML = "<br/><br/>";
-	let amb = a - b;
-        document.getElementById(lines[7]).innerHTML = "AC"+sqr+sign[0]+a+sign[2]+b+sign[0]+amb+"<br/>AC"+sign[0];
-    if(a-b > 0) {
-        radical = 0;
-        for(i = 0; i < 1000; i++) {
-            if(i*i == a-b) {
-               radical = i;
-               break; 
-            }
-        }
-        if(radical > 0) {
-                document.getElementById(lines[8]).innerHTML = radical;
-        }
-        else {
-                document.getElementById(lines[8]).innerHTML = "√"+amb;
-        }
-    }
-    else {
-            document.getElementById(lines[8]).innerHTML = no_radical;
-    }
-}
-function resetCoef() {
-	let j = 0;
-	do {
-		document.getElementById(lines[j]).innerHTML = ' ';
-		j = j + 1;
-	} while(j < 16);
-}
+});
