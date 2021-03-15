f71a457dad096671e349516ef5e44a4b34c49ff3
findsite <60389517+findsite@users.noreply.github.com>
2021-03-15 03:58:21 +0200

    Add files via upload

--- discriminant.js
+++ discriminant.js
@@ -1,216 +1,295 @@
-let lines = new Array("fr-line", "sc-line", "tr-line", "fu-line", "fv-line", "sx-line", "sv-line", "eg-line", "nn-line", "tn-line", "el-line", "tw-line", "tn-line", "fn-line", "sn-line", "svn-line", "en-line", "nnn-line");   
+$(function() {
 
-function setCoef() {
-   let a, b, c, sqr_b, a_mult_c, discriminant, inv_b, dbl_b;
-   let radical_of_discriminant, inv_a_mult_c, dbl_a_mult_c, s_a_mult_c;
-        a = prompt("Введите первый коэффициент");
-		b = prompt("Введите второй коэффициент");
-		c = prompt("Введите третий коэффициент");
-    dbl_b = b * 2;
-    inv_b = b - dbl_b;
-    sqr_b = b * b;
-    a_mult_c = 4 * a * c;
-    discriminant = sqr_b - a_mult_c;
-    if(a_mult_c >= 0) {
-            document.getElementById(lines[1]).innerHTML = "Через дискриминант:"+"<br/><br/>D = b^2 - 4 a c<br/>D = "+sqr_b+" - "+a_mult_c+" = "+discriminant;
-    }
-    if(a_mult_c < 0) {
-        s_a_mult_c = a_mult_c;
-        dbl_a_mult_c = s_a_mult_c * 2;
-        inv_a_mult_c = s_a_mult_c - dbl_a_mult_c;
-            document.getElementById(lines[1]).innerHTML = "Через дискриминант:"+"<br/><br/>D = b^2 - 4 a c<br/>D = "+sqr_b+" + "+inv_a_mult_c+" = "+discriminant;
-    }
-    
-        /* Find the radical of discriminant  */
-    
-        for(let i = 0; i < 50000; i = i + 1) {
-            let sqr = i * i;
-                if (sqr == discriminant) {
-                    radical_of_discriminant = i;
-                    break;
-                }
-				else {
-					radical_of_discriminant = 0;
-				}
-        }
-        
-        /* Find the unknown numbers */
-        
-        b = b - dbl_b;
-        a = a * 2;
-        
-        /* First x */
-        let x_one_numerator = b + radical_of_discriminant;
-        let x_one = x_one_numerator / a;
-        
-        /* Second x */
-        let x_two_numerator = b - radical_of_discriminant;
-        let x_two = x_two_numerator / a;
-        
-        /* Write all actions */
+	$('#sqr-add').click(function() {
+		$('#formula').val($('#formula').val() + '²');
+	});
+
+	$('#start').click(function() { $('#formula').val() != '' ? start() : alert('Введите формулу') });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	function signOf(num) {
+		return (num >= 0 ? ' + ' : ' - ');
+	}
+	function toPos(num) {
+		return Math.sqrt(Math.pow(num, 2));
+	}
+	function tryToDivide(a, b, c = a, r = 'value') {
+		let num = 0;
+		for(let i = Math.max(a, b, c); i > -Math.max(a, b, c); i--) {
+			if(a % i == 0 && b % i == 0 && c % i == 0 && i != 1 && i != -1) {
+				num = i;
+				break;
+			}
+		}
+		return (num != 0 ? (r == 'isCan' ? true : num) : false);
+	}
+	function radical(num) {
+		for(let i = num; i >= 0; i--) {
+			if(i * i == num) {
+				return i;
+			}
+		}
+
+		return -1;
+
+	}
+	function radicalDecay(rad) {
+		console.log(rad);
+		for(let i = rad - 1; i > 1; i--) {
+			if(Math.pow(radical(rad / i), 2) * i == rad) {
+				return (radical(rad / i) + '√' + i);
+			}
+			
+		}
+		return -1;
+	}
+	function countNumbersAfterDot(num) {
+		let numString = num.toString();
+
+		numString = numString.split('');
+		numString = numString.splice(numString.indexOf('.') + 1, numString.length);
+
+		return numString.length;
+	}
+	function isDividible(num, div) {
+		return countNumbersAfterDot(num / div) < 2 ? true : false;
+	}
+
+
+	function faction(nom, den) {
+		return '<div class="faction"> <div class="fc">' + nom + '</div> <br/> <div>' + den + '</div> </div>';
+	}
+
+
+
+	function start() {
+		!document.getElementById('solution') ? $('#container').append('<div id="solution"></div>') : false;
+
+		let f = $('#formula').val();
+
+		const block = $('#solution');
+		let text = '';
+
+		// Remove spaces
+		f = f.split('');
+		while(f.includes(' ')) {
+			f.splice(f.indexOf(' '), 1);
+		}
+		f = f.join('');
+
+		// Get coefficients
+		let coefs = getCoefficients(f);
+		if(coefs.length < 3) {
+			coefs.unshift(1 * (f[0] == '-' ? -1 : 1));
+		}
+		if(coefs.length < 3) {
+			coefs.splice(1, 0, 1 * (f[2] == '-' || f[3] == '-' ? -1 : 1));
+		}
+		let oldCoefs = new Array;
+		oldCoefs = oldCoefs.concat(coefs)
+
+
+
+		text += (f[0] == '-' ? '-' : '') + (Math.pow(coefs[0],2) == 1 ? '' : toPos(coefs[0])) + 'x²' + signOf(coefs[1]) + (Math.pow(coefs[1],2) == 1 ? '' : toPos(coefs[1])) + 'x' + signOf(coefs[2]) + toPos(coefs[2]) + ' = 0<br/>';
+
+		if(Math.pow(coefs[1],2) - 4 * coefs[0] * coefs[2] >= 0) {
+
+			if(f[0] == '-') {
+				text += '<br/>Умножим коэффициенты на -1: <br/>';
+
+				coefs[0] *= -1;
+				coefs[1] *= -1;
+				coefs[2] *= -1;
+
+				text += (Math.pow(coefs[0],2) == 1 ? '' : coefs[0]) + 'x²' + signOf(coefs[1]) + (Math.pow(coefs[1],2) == 1 ? '' : toPos(coefs[1])) + 'x' + signOf(coefs[2]) + toPos(coefs[2]) + ' = 0<br/>';
+			}
+			if(tryToDivide(coefs[0], coefs[1], coefs[2], 'isCan')) {
+				let divide = tryToDivide(coefs[0], coefs[1], coefs[2], 'getValue');
+				text += '<br/>Разделим коэффициенты на ' + divide + ':<br/>';
+
+				coefs[0] /= divide;
+				coefs[1] /= divide;
+				coefs[2] /= divide;
+
+				text += (Math.pow(coefs[0],2) == 1 ? '' : coefs[0]) + 'x²' + signOf(coefs[1]) + (Math.pow(coefs[1],2) == 1 ? '' : toPos(coefs[1])) + 'x' + signOf(coefs[2]) + toPos(coefs[2]) + ' = 0<br/>';
+			}
+
+			let rad = {
+				x1: (((coefs[1] * -1) + Math.sqrt(coefs[1] * coefs[1] - (4 * coefs[0] * coefs[2]))) / (coefs[0] * 2)),
+				x2: (((coefs[1] * -1) - Math.sqrt(coefs[1] * coefs[1] - (4 * coefs[0] * coefs[2]))) / (coefs[0] * 2))
+			}
+
+			text += "<p>";
+			if(coefs[0] == 1 && (rad.x1 % 1 == 0 && rad.x2 % 1 == 0)) {
+				text += "<br/><br/>Так как коэффициент 'a' = 1, решаем через теорему Виета:<br/><br/>";
+				text += 'x<sub>1</sub> + x<sub>2</sub> = ' + (coefs[1] * -1) + '<br/>';
+				text += 'x<sub>1</sub> ⋅ x<sub>2</sub> = ' + coefs[2] + '<br/>';
+				text += 'x<sub>1</sub> = ' + rad.x1 + '<br/>';
+				text += 'x<sub>2</sub> = ' + rad.x2 + '<br/></p>';
+
+			}
+			else {
+				text += '<br/><br/>Решаем через дискриминант:<br/><br/>';
+				text += 'D = b<sup>2</sup> - 4ac = ' + Math.pow(coefs[1], 2) + signOf(-4 * coefs[0] * coefs[2]) + toPos(-4 * coefs[0] * coefs[2]) + ' = ' + (Math.pow(coefs[1],2) - (4 * coefs[0] * coefs[2])) + '<br/>';
 				
-            if(discriminant >= 0) {
-                if(radical_of_discriminant == 0 && discriminant > 0) {
-						document.getElementById(lines[2]).innerHTML = "vD = v"+discriminant;
-                        document.getElementById(lines[6]).innerHTML = "X1 = "+x_two+" + v"+discriminant;
-                }
-                if(radical_of_discriminant > 0 && discriminant > 0) {
-					    document.getElementById(lines[2]).innerHTML = "vD = "+radical_of_discriminant;
-                        document.getElementById(lines[6]).innerHTML = "X1 = "+x_two;
-                }
-                if(radical_of_discriminant == 0 && discriminant == 0) {
-						document.getElementById(lines[2]).innerHTML = "vD = "+radical_of_discriminant;
-                        document.getElementById(lines[6]).innerHTML = "X1 = "+x_two;
-                }
-                    
-                if(radical_of_discriminant == 0 && discriminant > 0) {
-                        document.getElementById(lines[7]).innerHTML = "X2 = "+x_one+" - v"+discriminant; 
-                }
-                if(radical_of_discriminant > 0 && discriminant > 0) {
-                        document.getElementById(lines[7]).innerHTML = "X2 = "+x_one;
-                }
-                if(radical_of_discriminant == 0 && discriminant == 0) {
-                        document.getElementById(lines[7]).innerHTML = "X2 = "+x_two;
-                }
-                
-            
-            /* Linear factors */
-                
-                let sign_x_one = " - ";
-                let sign_x_two = " - ";
-                let sqr_linear_factors = 0;
-                let dbl_x, inv_x_one, inv_x_two;
-                inv_x_one = x_one;
-                inv_x_two = x_two;
-                if(x_one < 0) {
-                    sign_x_one = " + ";
-                    dbl_x = x_one * 2;
-                    inv_x_one = x_one - dbl_x;
-                }
-                if(x_two < 0) {
-                    sign_x_two = " + ";
-                    dbl_x = x_two * 2;
-                    inv_x_two = x_two - dbl_x;
-                }
-                if(x_one == x_two && sign_x_one == sign_x_two) {
-                	sqr_linear_factors = 1;
-                }
-                
-                a = a / 2;
-				let l_f = "<br/><br/>Разложение на линейные множители:<br/><br/>";
-                    if(radical_of_discriminant > 0 && discriminant > 0) {
-                        if(a >= 0 || a < 0) {
-                            if(a != -1 && a != 1) {
-                            	if(sqr_linear_factors == 0) {
-										document.getElementById(lines[8]).innerHTML = l_f;
-                                    	document.getElementById(lines[11]).innerHTML =  "(x"+sign_x_one+inv_x_one+")";
-                                        document.getElementById(lines[12]).innerHTML = a+"\r(x"+sign_x_two+inv_x_two+")";
-                                }
-                                else {
-										document.getElementById(lines[8]).innerHTML = l_f;
-                                		document.getElementById(lines[11]).innerHTML = "(x"+sign_x_one+inv_x_one+")^2";
-                                }
-                            }
-                        }
-                       	if(a == -1) {
-                       		if(sqr_linear_factors == 0) {
-									document.getElementById(lines[8]).innerHTML = l_f;
-	                       	        document.getElementById(lines[11]).innerHTML = "-"+"(x"+sign_x_one+inv_x_one+")";
-    	                   	        document.getElementById(lines[12]).innerHTML = "\r(x"+sign_x_two+inv_x_two+")";                            
-        	                }
-        	                else {
-									document.getElementById(lines[8]).innerHTML = l_f;
-        	                		document.getElementById(lines[11]).innerHTML = "-(x"+sign_x_one+inv_x_one+")^2";
-        	                }
-        	            }
-                        if(a == 1) {
-                        	if(sqr_linear_factors == 0) {
-									document.getElementById(lines[8]).innerHTML = l_f;
-	                                document.getElementById(lines[11]).innerHTML = "(x"+sign_x_one+inv_x_one+")";
-    	                            document.getElementById(lines[12]).innerHTML = "\r(x"+sign_x_two+inv_x_two+")";  
+				let D = (Math.pow(coefs[1],2) - (4 * coefs[0] * coefs[2]));
+				text += '√D = ' + (radical(D) != -1 ? radical(D) : (radicalDecay(D) != -1 ? '√' + D + ' = ' + radicalDecay(D) : ' √' + D)) +'<br/>';
+				D = (radical(D) != -1 ? radical(D) : (radicalDecay(D) != -1 ? radicalDecay(D) : '√' + D));
+
+				text += faction('', ' x<sub> 1</sub> = ') + faction('-b + √D', '2a') + ' = ';
+				text += faction((-coefs[1] + ' + ' + D), (coefs[0] * 2));
+				rad.x1 = faction((-coefs[1] + ' + ' + D), (coefs[0] * 2));
+
+				if(D[0] % 1 != 0) {
+					if(D % 1 != 0) {
+						if(tryToDivide(coefs[0] * 2, -coefs[1], coefs[0], 'isCan')) {
+							if(coefs[0] * 2 > -coefs[1]) {
+								let div = tryToDivide(coefs[0] * 2, -coefs[1]);
+
+								text += ' = ' + faction(-coefs[1] / div + ' + ' + D, coefs[0] * 2 / div);
+								rad.x1 = faction(-coefs[1] / div + ' + ' + D, coefs[0] * 2 / div);
 							}
 							else {
-									document.getElementById(lines[8]).innerHTML = l_f;
-									document.getElementById(lines[11]).innerHTML = "(x"+sign_x_one+inv_x_one+")";
+								let div = tryToDivide(-coefs[1], coefs[0] * 2);
+
+								text += ' = ' + (-coefs[1] / div) + ' + ' + D;
+								rad.x1 = (-coefs[1] / div) + ' + ' + D;
 							}
-                        }
-                    }
-                    if(radical_of_discriminant == 0 && discriminant == 0) {
-                        if(a >= 0 || a < 0) {
-                            if(a != -1 && a != 1) {
-                            	if(sqr_linear_factors == 0) {
-										document.getElementById(lines[8]).innerHTML = l_f;
-                                    	document.getElementById(lines[11]).innerHTML = "(x"+sign_x_one+inv_x_one+")";
-                                        document.getElementById(lines[12]).innerHTML = a+"\r(x"+sign_x_two+inv_x_two+")";
-                                }
-                                else {
-										document.getElementById(lines[8]).innerHTML = l_f;
-                                		document.getElementById(lines[11]).innerHTML = "(x"+sign_x_one+inv_x_one+")^2";
-                                }
-                            }
-                        }
-                       	if(a == -1) {
-                       		if(sqr_linear_factors == 0) {
-									document.getElementById(lines[8]).innerHTML = l_f;
-	                       	        document.getElementById(lines[11]).innerHTML = "-(x"+sign_x_one+inv_x_one+")";
-    	                   	        document.getElementById(lines[12]).innerHTML = "\r(x"+sign_x_two+inv_x_two+")";                            
-        	                }
-        	                else {
-									document.getElementById(lines[8]).innerHTML = l_f;
-        	                		document.getElementById(lines[11]).innerHTML = "-(x"+sign_x_one+inv_x_one+")^2";
-        	                }
-        	            }
-                        if(a == 1) {
-                        	if(sqr_linear_factors == 0) {
-									document.getElementById(lines[8]).innerHTML = l_f;
-	                                document.getElementById(lines[11]).innerHTML = "(x"+sign_x_one+inv_x_one+")";
-    	                            document.getElementById(lines[12]).innerHTML = "\r(x"+sign_x_two+inv_x_two+")";  
+						}	
+
+					}
+					else {
+						let cd = -coefs[1] + D;
+						text += ' = ' + faction(cd, coefs[0] * 2);
+						if(cd > coefs[0] * 2) {
+							if(tryToDivide(cd, coefs[0] * 2, cd, 'isCan')) {
+								let div = tryToDivide(cd, coefs[0] * 2);
+
+								if(coefs[0] * 2 / div == 1) {
+									text += ' = ' + cd / div;
+									rad.x1 = cd / div;
+								}
+								else {
+									text += ' = ' + faction(cd / div, coefs[0] * 2 / div);
+									rad.x1 = faction(cd / div, coefs[0] * 2 / div);
+								}
+							}
+						}
+						else {
+							if(tryToDivide(coefs[0] * 2, cd, coefs[0] * 2, 'isCan')) {
+								let div = tryToDivide(coefs[0] * 2, cd);
+
+								text += ' = ' + faction(cd / div, coefs[0] * 2 / div);
+								rad.x1 = faction(cd / div, coefs[0] * 2 / div);
+
+								if(isDividible(cd / div, coefs[0] * 2 / div)) {
+									text += ' = ' + (cd / div) / (coefs[0] * 2 / div);
+									rad.x1 = (cd / div) / (coefs[0] * 2 / div);
+								}
+							}
+						}
+					}
+				}
+				text += '<br/>';
+				text += faction('', ' x<sub> 2</sub> = ') + faction('-b - √D', '2a') + ' = ';
+				text += faction((-coefs[1] + ' - ' + D), (coefs[0] * 2));
+				rad.x2 = faction((-coefs[1] + ' - ' + D), (coefs[0] * 2));
+
+				if(D[0] % 1 != 0) {
+						if(D % 1 != 0) {
+							if(tryToDivide(coefs[0] * 2, -coefs[1], coefs[0], 'isCan')) {
+								if(coefs[0] * 2 > -coefs[1]) {
+									let div = tryToDivide(coefs[0] * 2, -coefs[1]);
+
+									text += ' = ' + faction(-coefs[1] / div + ' - ' + D, coefs[0] * 2 / div);
+									rad.x2 = faction(-coefs[1] / div + ' - ' + D, coefs[0] * 2 / div);
+								}
+								else {
+									let div = tryToDivide(-coefs[1], coefs[0] * 2);
+
+									text += ' = ' + (-coefs[1] / div) + ' - ' + D;
+									rad.x2 = (-coefs[1] / div) + ' - ' + D;
+								}
+							}	
+
+						}
+						else {
+							let cd = -coefs[1] - D;
+							text += ' = ' + faction(cd, coefs[0] * 2);
+							rad.x2 = faction(cd, coefs[0] * 2);
+
+							if(cd > coefs[0] * 2) {
+								if(tryToDivide(cd, coefs[0] * 2, cd, 'isCan')) {
+									let div = tryToDivide(cd, coefs[0] * 2);
+
+									if(coefs[0] * 2 / div == 1) {
+										text += ' = ' + cd / div;
+										rad.x2 = cd / div;
+									}
+									else {
+										text += ' = ' + faction(cd / div, coefs[0] * 2 / div);
+										rad.x2 = faction(cd / div, coefs[0] * 2 / div);
+									}
+								}
 							}
 							else {
-									document.getElementById(lines[8]).innerHTML = l_f;
-									document.getElementById(lines[11]).innerHTML = "(x"+sign_x_one+inv_x_one+")^2";
+								if(tryToDivide(coefs[0] * 2, cd, coefs[0] * 2, 'isCan')) {
+									let div = tryToDivide(coefs[0] * 2, cd);
+
+									text += ' = ' + faction(cd / div, coefs[0] * 2 / div);
+									rad.x2 = faction(cd / div, coefs[0] * 2 / div);
+
+									if(isDividible(cd / div, coefs[0] * 2 / div)) {
+										text += ' = ' + (cd / div) / (coefs[0] * 2 / div);
+										rad.x2 = (cd / div) / (coefs[0] * 2 / div);
+									}
+								}
 							}
-                        }
-                    }
-                    if(radical_of_discriminant == 0 && discriminant >= 0) {
-                        if(a > 1 || a < 1) {
-                            if(a != -1 && a != 1) {
-										document.getElementById(lines[8]).innerHTML = l_f;
-										document.getElementById(lines[11]).innerHTML = "(x"+sign_x_one+"("+inv_x_one+"-"+"v"+discriminant+"))";
-                                    	document.getElementById(lines[12]).innerHTML = a+"\r(x"+sign_x_one+"("+inv_x_one+"+"+"v"+discriminant+"))";
-                            }
-                        }
-                        if(a == -1) {
-									document.getElementById(lines[8]).innerHTML = l_f;
-                                	document.getElementById(lines[11]).innerHTML = "-"+"(x"+sign_x_one+"("+inv_x_one+"-"+"v"+discriminant+"))";
-                                	document.getElementById(lines[12]).innerHTML = "\r(x"+sign_x_one+"("+inv_x_one+"+"+"v"+discriminant+"))";
-                        }
-                        if(a == 1) {
-								document.getElementById(lines[8]).innerHTML = l_f;
-                                document.getElementById(lines[11]).innerHTML = "(x"+sign_x_one+"("+inv_x_one+"-"+"v"+discriminant+"))";
-                                document.getElementById(lines[12]).innerHTML = "\r(x"+sign_x_one+"("+inv_x_one +"+"+"v"+discriminant+"))";  
-                        }
-                    }
-                    
-                    /* vieta theorem */
-                    
-                    if(a == 1 && radical_of_discriminant > 0) {                     
-                        document.getElementById(lines[14]).innerHTML = "<br/><br/>Через теорему Виета:<br/><br/>X1 + X2 = "+inv_b;
-                        document.getElementById(lines[15]).innerHTML = "<br/>X1 * X2 = "+c;
-                        document.getElementById(lines[16]).innerHTML = "X1 = "+x_one+"<br/>X2 = "+x_two;
-                    }
-		    else {
-			document.getElementById(lines[14]).innerHTML = "<br/><br/>Невозможно решить через теорему Виета.";    
-		    }
-            }
-            else {
-                    document.getElementById(lines[3]).innerHTML = "Корней нет";
-            }
-}
-function resetCoef() {
-	let j = 0;
-	do {
-		document.getElementById(lines[j]).innerHTML = ' ';
-		j = j + 1;
-	} while(j < 17);
-}
+						}
+					}
+				}
+
+			text += "</p><br/><br/>";
+
+			coefs = oldCoefs;
+
+			text += 'Разложение на линейные множители:<br/>'
+			text += (f[0] == '-' ? '-' : '') + (Math.pow(coefs[0],2) == 1 ? '' : toPos(coefs[0])) + 'x²' + signOf(coefs[1]) + (Math.pow(coefs[1],2) == 1 ? '' : toPos(coefs[1])) + 'x' + signOf(coefs[2]) + toPos(coefs[2]) + ' = ';
+			text += coefs[0] + '(x' + signOf(rad.x1) + (rad.x1 % 1 == 0 ? toPos(rad.x1) : rad.x1) + ')(x' + signOf(rad.x2) + (rad.x2 % 1 == 0 ? toPos(rad.x2) : rad.x2) + ')'; 
+		}
+		else {
+			text += 'Уравнение не имеет корней.';
+		}
+		
+
+		block.html(text);
+		console.log(coefs)
+	}
+
+	// Function that gets an coefficients of the formula
+	function getCoefficients(f) {
+		return f.match(/(-?\d+(\.\d+)?)/g).map(n => +n);
+	}
+
+
+
+
+});
