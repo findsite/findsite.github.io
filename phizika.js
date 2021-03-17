b520b3118ff7d8364ce83e0d3f987f766e3acdd3
findsite <60389517+findsite@users.noreply.github.com>
2021-03-17 18:39:01 +0200

    Update phizika.js

--- phizika.js
+++ phizika.js
@@ -5,10 +5,11 @@ const Title = 'Физика';
 
 document.title = Title;
 
+let lang = getLang();
 
 
 let bricks = {
-	names: ['Ядерные реакции'],
+	names: [lang.phizika[0]],
 	imgURLS: ['https://cdn.discordapp.com/attachments/521767712792379414/801907297600405504/96668c8bbbb6bc70.jpg'],
 	links: ['yadernaya-reakziya.html'],
 	opacity: ['1']
@@ -66,5 +67,6 @@ if(theme != 0) {
 	document.getElementById('html').style = 'background-color: #111';
 	document.getElementById('header').style = 'background-color: #333; border-color: #333 #333 #444 #333';
 	document.getElementById('home').src = 'https://cdn.discordapp.com/attachments/521767712792379414/691641762963259481/13.jpg';
-	document.getElementById('findsite').src = 'https://cdn.discordapp.com/attachments/521767712792379414/691640002202959942/findsitesmall.jpg';
+	document.getElementById('findsite').src = 'https://cdn.discordapp.com/attachments/521767712792379414/691640002202959942/findsitesmall.jpg;
 }
+
