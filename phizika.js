c23c0024d08fd8fdc7a035a8f66cfad5f04f3c97
findsite <60389517+findsite@users.noreply.github.com>
2021-05-19 00:07:04 +0300

    Update phizika.js

--- phizika.js
+++ phizika.js
@@ -1,9 +1,19 @@
 for(let i = 0; i < $('.slide').length; i++) {
 	$('.slide').eq(i).css('height', window.innerHeight + 2);
 	document.getElementsByClassName('slide')[i].onclick = function() { 
-		let currentNumber = Number($('#counter').text());
-		if($('.slide').length > currentNumber) $('#counter').text(Number($('#counter').text()) + 1);
-
-		$('html, body').animate({ scrollTop: (window.innerHeight * (i + 1) + 2 * (i + 1)) });
+		$('#input').val(' ');
 	}
 }
+
+let count = 0;
+
+function checkValue() {
+	if($('#input').val() == ' ') {
+		count++;
+		$('#input').val('');
+		if($('.slide').length > Number($('#counter').text())) $('#counter').text(Number($('#counter').text()) + 1);
+		$('html, body').animate({ scrollTop: window.innerHeight * count + 2 * count });
+	}
+}
+
+setInterval(()=>{checkValue()}, 1);
