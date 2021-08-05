634d26ff40168e3a8e7aa154c3ad8e5b8174fd96
findsite <60389517+findsite@users.noreply.github.com>
2021-08-05 03:22:16 +0300

    Add files via upload

--- books.js
+++ books.js
@@ -1,30 +1,45 @@
+
+setTimeout( () => { $('ul').eq(0).animate({ opacity: 1 }, animateTimeMS) }, animateTimeMS); 
+
+
+
 let books = [
 	'https://drive.google.com/file/d/1Nr_1-GGD4MX5msIm9uWb3t_3nVqueNId/preview',
+	'https://drive.google.com/file/d/1wwdpuhk6Ags0a8Gegr3mpG-In2SIlJSN/preview',
 ]
 let lessonNames = [
 	'Алгебра',
+	'Геометрия'
+]
+let previewURL = [
+	'https://cdn.discordapp.com/attachments/602251277879214080/872102662341017680/unknown.png',
+	'https://cdn.discordapp.com/attachments/602251277879214080/872240042054131783/unknown.png'
 ]
 
 
 let rowCount = -1;
 
 for(let i = 0; i < books.length; i++) {
-	if(i % 3 == 0) {
-		$('#books-list').append('<ul></ul>');
+	if(i % 2 == 0) {
+		$('#books-list').append('<ul class="books-block"></ul>');
 		rowCount++;
 	}
 	let nLi = document.createElement('li')
+	let nImg = document.createElement('img')
 	let nP = document.createElement('p')
+	nImg.src = previewURL[i];
 	nP.innerText = lessonNames[i];
 	nLi.onclick = function() {
 		setBook(books[i]);
 	}
 
 	$('#books-list ul').eq(rowCount).append(nLi);
+	nLi.append(nImg);
 	nLi.append(nP);
 }
 
 function setBook(link) {
+	$('#books-list').css('margin', '0');
 	$('#books-list ul').remove();
-	$('#books-list').append('<iframe src="' + link + '" width="' + window.innerWidth + '" height="' + (window.innerHeight - $('header').innerHeight()) + '"></iframe>')
+	$('#books-list').append('<iframe src="' + link + '" width="' + window.innerWidth + '" height="' + (window.innerHeight - $('header').innerHeight()) + '" autoplay="allow"></iframe>')
 }
--- online-note.js
+++ online-note.js
@@ -1,5 +1,16 @@
 $('#choose-list').css('height', window.innerHeight - $('header').innerHeight());
 
 for(let i = 0; i < 2; i++) {
+	document.getElementsByTagName('li')[i].onmouseover = function() {
+		$('img').eq(i).css('animation', 'li-out .6s forwards');
+		$('p').eq(i).css('animation', 'li-p-out .6s forwards');
+	}
+	document.getElementsByTagName('li')[i].onmouseout = function() {
+		$('img').eq(i).css('animation', 'li-in .4s forwards');
+		$('p').eq(i).css('animation', 'li-p-in .6s forwards');
+	}
+}
+
+for(let i = 0; i < 2; i++) {
 	$('#choose-list li').eq(i).animate({opacity: 1}, animateTimeMS);
 }
